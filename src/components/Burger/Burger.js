87b104ad0ae3f01ede4541f2fd531ff74c1a7a81
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-16 17:02:46 +1000

    Initial commit of the Burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient type={igKey} key={igKey + i } />;
+        });
+    });
+    return(
+        <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+        <BurgerIngredient type="bread-bottom" />    
+        </div>
+    );
+};
+
+export default burger;
