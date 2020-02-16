cd141d7674f0bfae50b9bec80a5dd3d904aeb10f
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-16 17:17:46 +1000

    Set the initial state to empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient type={igKey} key={igKey + i } />;
         });
-    });
+    }).reduce((arr,el) => {
+        return arr.concat(el);
+    }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients to your burger.</p>;
+    }
     return(
         <div className={classes.Burger}>
         <BurgerIngredient type="bread-top" />
