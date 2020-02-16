8e540707e4a5832ea9594e1a0b4e8462298b1b13
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-16 19:30:36 +1000

    Add Order Now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: true,
+    }
+
+    updatePurchaseState = () => {
+        const ingredients = {
+            ...this.state.ingredients
+        };
+        const sum = Object.keys(ingredients).map(igKey => {return ingredients[igKey]})
+        .reduce((sum, el) => {
+            return sum + el;
+        },0);
+        this.setState({purchaseable: sum > 0});
     }
 
     addIngredientHander = (type) => {
@@ -31,6 +43,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState();
     }
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -46,6 +59,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState();
     }
     render() {
         const disabledInfo = {
@@ -61,6 +75,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHander}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
                     price={this.state.totalPrice}
                 />
             </React.Fragment>
