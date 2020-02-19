3b6701ab6bd767f7034bd7dc16b5cd7a1beb9159
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-19 19:36:46 +1000

    Start working on the toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from '../Toolbar/Toolbar.module.css';
+
+const toolbar = (props) => (
+	<header className={classes.Toolbar}>
+		<div>MENU</div>
+		<div>Logo</div>
+		<nav>...</nav>
+	</header>
+);
+
+export default toolbar;
