ef6ec65d1b2d46d964cf3a5f49386a1a71f1d0fe
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-19 20:16:07 +1000

    Add basic navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from '../Toolbar/Toolbar.module.css';
 import Logo from '../../../components/Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
-		<div>MENU</div>
+		<div>Menu</div>
 		<Logo />
-		<nav>...</nav>
+		<nav><NavigationItems /></nav>
 	</header>
 );
 
