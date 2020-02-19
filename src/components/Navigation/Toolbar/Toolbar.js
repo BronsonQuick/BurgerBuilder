60d2bc3627f7f5063b8c616665ef51ea80fc6c75
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-19 19:48:00 +1000

    Add logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from '../Toolbar/Toolbar.module.css';
+import Logo from '../../../components/Logo/Logo';
 
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
-		<div>Logo</div>
+		<Logo />
 		<nav>...</nav>
 	</header>
 );
