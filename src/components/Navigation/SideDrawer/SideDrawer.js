42b34c6f30ad1ad09d34fa878f24e3d5c28530c7
Bronson Quick <bronson@bronsonquick.com.au>
2020-02-20 14:53:42 +1000

    Adjust the responsive CSS styles

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NagivationItems from '../NavigationItems/NavigationItems';
+import classes from '../SideDrawer/SideDrawer.module.css';
+
+const sideDrawer = (props) => {
+	return(
+		<div className={classes.SideDrawer}>
+			<div className={classes.Logo}>
+				<Logo />
+			</div>
+			<nav>
+				<NagivationItems />
+			</nav>
+		</div>
+	);
+};
+export default sideDrawer;
