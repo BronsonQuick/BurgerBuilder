318277c92cde7fe58b2fa4ce5a1c50abeb3baf52
Bronson Quick <bronson@bronsonquick.com.au>
2020-03-21 20:44:00 +1000

    Add redirects

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -29,6 +29,11 @@ class Auth extends Component {
 		},
 		isSignUp: true
 	}
+	componentDidMount() {
+		if ( !this.props.buildingBurger && this.props.authRedirectPath !== '/' ) {
+			this.props.onSetAuthRedirectPath();
+		}
+	}
 	inputChangedHandler = (event, controlName) => {
 		const updatedControls = {
 			...this.state.controls,
@@ -53,7 +58,7 @@ class Auth extends Component {
 	render() {
 		let authRedirect = null;
 		if ( this.props.isAuthenticated ) {
-			authRedirect = <Redirect to="/" />;
+			authRedirect = <Redirect to={this.props.authRedirectPath} />;
 		}
 		const formElementsArray = [];
 		for ( let key in this.state.controls ) {
@@ -106,13 +111,16 @@ const mapStateToProps = state => {
 	return {
 		loading: state.auth.loading,
 		error: state.auth.error,
-		isAuthenticated: state.auth.token !== null
+		isAuthenticated: state.auth.token !== null,
+		buildingBurger: state.burgerBuilder.building,
+		authRedirectPath: state.auth.authRedirectPath
 	}
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onAuth: (email, password, isSignUp ) => dispatch(actions.auth(email, password, isSignUp))
+		onAuth: (email, password, isSignUp ) => dispatch(actions.auth(email, password, isSignUp)),
+		onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
 	}
 };
 export default connect( mapStateToProps, mapDispatchToProps)(Auth);
