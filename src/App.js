6195361d9acc3cb365e14a9311fb7a43eae7deab
Bronson Quick <bronson@bronsonquick.com.au>
2020-03-23 10:34:41 +1000

    Persist session data in local storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actionTypes from './store/actions/index';
 
-import { Route } from 'react-router-dom';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/orders" component={Orders} />
-      </Layout>
-    </div>
-  );
+import { Route, withRouter } from 'react-router-dom';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSign();
+  }
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/orders" component={Orders} />
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSign: () => dispatch(actionTypes.authCheckState())
+  };
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
