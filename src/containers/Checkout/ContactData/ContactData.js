40a6e2d98edb75fdea1b77eb6a86d9290f59e3e9
Bronson Quick <bronson@bronsonquick.com.au>
2020-03-30 11:34:50 +1000

    Convert Contact Data to use hooks

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from '../../../components/UI/Input/Input';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -8,119 +8,115 @@ import { connect } from 'react-redux';
 import styles from './ContactData.module.css';
 import * as actions from '../../../store/actions/index';
 
-class ContactData extends Component {
-	state = {
-		orderForm: {
-				name: {
-					elementType: 'input',
-					elementConfig: {
-						type: 'text',
-						placeholder: 'Your Name'
-					},
-					value: ''
+function ContactData(props) {
+	const [orderForm, setOrderForm ] = useState({
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name'
 				},
-				street: {
-					elementType: 'input',
-					elementConfig: {
-						type: 'text',
-						placeholder: 'Street'
-					},
-					value: ''
+				value: ''
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
 				},
-				zipCode: {
-					elementType: 'input',
-					elementConfig: {
-						type: 'text',
-						placeholder: 'Postcode'
-					},
-					value: ''
+				value: ''
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Postcode'
 				},
-				country: {
-					elementType: 'input',
-					elementConfig: {
-						type: 'text',
-						placeholder: 'Your Country'
-					},
-					value: ''
+				value: ''
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Country'
 				},
-				email: {
-					elementType: 'input',
-					elementConfig: {
-						type: 'email',
-						placeholder: 'Your Email'
-					},
-					value: ''
+				value: ''
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your Email'
 				},
-				deliveryMethod: {
-					elementType: 'select',
-					elementConfig: {
-						options: [
-							{value: 'fastest', displayValue: 'Fastest'},
-							{value: 'cheapest', displayValue: 'Cheapest'},
-						]
-					},
-					value: 'fastest'
-				}
+				value: ''
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{value: 'fastest', displayValue: 'Fastest'},
+						{value: 'cheapest', displayValue: 'Cheapest'},
+					]
+				},
+				value: 'fastest'
 		}
-	}
-	orderHandler = (event) => {
+	});
+	const orderHandler = (event) => {
 		event.preventDefault();
 		const formData = {};
-		for ( let formElementIdentifier in this.state.orderForm ) {
-			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+		for ( let formElementIdentifier in orderForm ) {
+			formData[formElementIdentifier] = orderForm[formElementIdentifier].value;
 		}
 
 		const order = {
-			ingredients: this.props.ings,
-			price: this.props.price,
+			ingredients: props.ings,
+			price: props.price,
 			orderData: formData,
-			userId: this.props.userId
+			userId: props.userId
 		}
 
-		this.props.onOrderBurger(order, this.props.token);
+		props.onOrderBurger(order, props.token);
 	}
-	inputChangedHandler = (event, inputIdentifier) => {
+	const inputChangedHandler = (event, inputIdentifier) => {
 		const updatedOrderForm = {
-			...this.state.orderForm
+			...orderForm
 		}
 		const updatedFormElement = {
 			...updatedOrderForm[inputIdentifier]
 		}
 		updatedFormElement.value = event.target.value;
 		updatedOrderForm[inputIdentifier] = updatedFormElement;
-		this.setState({orderForm: updatedOrderForm })
+		setOrderForm(updatedOrderForm);
 	}
-	render() {
-		const formElementsArray = [];
-		for ( let key in this.state.orderForm ) {
-			formElementsArray.push({
-				id: key,
-				config: this.state.orderForm[key]
-			});
-		}
-		let form = (
-			<form>
-					{formElementsArray.map( formElement => (
-						<Input
-							key={formElement.id}
-							elementType={formElement.config.elementType}
-							elementConfig={formElement.config.elementConfig}
-							value={formElement.config.value}
-							changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-					))}
-					<Button btnType="Success" clicked={this.orderHandler}>Order</Button>
-				</form>
-		);
-		if (this.props.loading) {
-			form = <Spinner />;
-		}
-		return(
-			<div className={styles.ContactData} >
-				<h4>Enter your contact data</h4>
-				{form}
-			</div>
-		);
+	const formElementsArray = [];
+	for ( let key in orderForm ) {
+		formElementsArray.push({
+			id: key,
+			config: orderForm[key]
+		});
 	}
+	let form = (
+		<form>
+				{formElementsArray.map( formElement => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						changed={(event) => inputChangedHandler(event, formElement.id)} />
+				))}
+				<Button btnType="Success" clicked={orderHandler}>Order</Button>
+			</form>
+	);
+	if (props.loading) {
+		form = <Spinner />;
+	}
+	return(
+		<div className={styles.ContactData} >
+			<h4>Enter your contact data</h4>
+			{form}
+		</div>
+	);
 }
 
 const mapDispatchToProps = dispatch => {
